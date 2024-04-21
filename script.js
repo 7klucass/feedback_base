329cc9462301fc3394b088443236639f8ee046ad
lucas soares <dossantos.lucas@eaportal.org>
2024-04-21 02:47:14 +0000

    Javascript do index

---
+++ script.js
@@ -0,0 +1,9 @@
+let botoes = document.querySelectorAll(".botoes button")
+botoes.forEach(botao =>{
+    botao.addEventListener(click, guardarNotas)
+})
+
+function guardarNotas(evento){
+    let nota = evento.target.innerText
+    localStorage.setItem("nota", nota)
+}
