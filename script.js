e21123d2fefdb88ed3089a4163a8300bf2f7ec23
lucas soares <dossantos.lucas@eaportal.org>
2024-04-24 18:40:44 +0000

    Correção

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let botoes = document.querySelectorAll(".botoes button")
-botoes.forEach(botao =>{
-    botao.addEventListener(click, guardarNotas)
+botoes.forEach(botao => {
+    botao.addEventListener("click", guardarNota)
 })
 
-function guardarNotas(evento){
+function guardarNota(evento) {
     let nota = evento.target.innerText
-    localStorage.setItem("nota", nota)
+
+localStorage.setItem("nota", nota)
 }
